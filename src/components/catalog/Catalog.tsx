1c38863c994eb581ff512897c1fd89cecc3b19a9
Maksim Tananykin <maks.tananykin.20177@mail.ru>
2022-11-04 23:51:42 +0300

    init

---
+++ src/components/catalog/Catalog.tsx
@@ -0,0 +1,13 @@
+export default function Catalog() {
+  const cards = [
+    {
+      id: "1",
+      name: "Card 1",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, alias!",
+      price: "10$",
+    },
+  ];
+
+  return <div></div>;
+}
